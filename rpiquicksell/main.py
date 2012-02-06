8bfff45d53c5de969bc0155bc17d46e924c2602a
Andrew Wang <zxwandrew@gmail.com>
2012-02-06 17:42:23 -0500

    helloworld3

--- rpiquicksell/main.py
+++ rpiquicksell/main.py
@@ -20,7 +20,7 @@ from google.appengine.ext.webapp import util
 
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world2!')
+        self.response.out.write('Hello world3!')
 
 
 def main():
