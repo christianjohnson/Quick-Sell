7e3e1b7b5d64fffa09eac1064ece96ec7270a5f5
Christian Johnson <christian@christianjohnson.org>
2012-03-22 20:42:49 -0400

    Moved to Python2.7

--- rpiquicksell/main.py
+++ rpiquicksell/main.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python
 
+import jinja2
 import os
-import cgi
-import datetime
-import urllib
-import wsgiref.handlers
+import webapp2
 
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
 from google.appengine.api import users
-from google.appengine.ext.webapp import template
 
 import models
 
-class MainHandler(webapp.RequestHandler):
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler):
   def get(self):
     if users.get_current_user():
 			url = users.create_logout_url(self.request.uri)
@@ -26,13 +24,13 @@ class MainHandler(webapp.RequestHandler):
 
     template_values = {
       'user': user,
-      'url_linktext': url_linktext,
+      'url_linktext': url_linktext
     }
 
-    path = os.path.join(os.path.dirname(__file__), 'index.html')
-    self.response.out.write(template.render(path, template_values))
+    template = jinja_environment.get_template('index.html')
+    self.response.out.write(template.render(template_values))
     
-class BrowseBooks(webapp.RequestHandler):
+class BrowseBooks(webapp2.RequestHandler):
   def get(self):
     if users.get_current_user():
 			url = users.create_logout_url(self.request.uri)
@@ -51,15 +49,29 @@ class BrowseBooks(webapp.RequestHandler):
       'books': books
     }
 
-    path = os.path.join(os.path.dirname(__file__), 'books.html')
-    self.response.out.write(template.render(path, template_values))
+    template = jinja_environment.get_template('browse.html')
+    self.response.out.write(template.render(template_values))
 
-application = webapp.WSGIApplication([('/', MainHandler),
-                                      ('/books', BrowseBooks),],
-                                            debug=True)
+class SellBooks(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    if user:
+  		url = users.create_logout_url(self.request.uri)
+  		url_linktext = 'Logout'
+  		user = users.get_current_user()
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
 
-def main():
-  util.run_wsgi_app(application)
+    template_values = {
+      'user': user,
+      'url_linktext': url_linktext,
+    }
 
-if __name__ == '__main__':
-  main()
+    template = jinja_environment.get_template('sell.html')
+    self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/browse', BrowseBooks),
+                               ('/sell', SellBooks)], 
+                               debug=True)
+
